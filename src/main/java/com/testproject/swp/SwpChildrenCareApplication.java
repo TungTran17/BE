9f22b80076e43491ff1a767f31615b8e1d404834
TungTran17 <tungtvhe163636@fpt.edu.vn>
2023-06-28 02:15:20 +0700

    update file application

--- src/main/java/com/testproject/swp/SwpChildrenCareApplication.java
+++ src/main/java/com/testproject/swp/SwpChildrenCareApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SwpApplication {
+public class SwpChildrenCareApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SwpApplication.class, args);
+		SpringApplication.run(SwpChildrenCareApplication.class, args);
 	}
 
 }
